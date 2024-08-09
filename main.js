36d8a2c2fc636015fe806260fc6ebf6b38d66ac3
IndraSteam <69793178+IndraSteam@users.noreply.github.com>
2024-08-09 23:43:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*window.alert = function(message, timeout=null){
+    const alert = document.createElement('div');
+    const alertButton = document.createElement('button');
+    alertButton.innerText = "OK";
+    alert.classList.add('alert');
+    alert.setAttribute('style',`
+        position:fixed;
+        top:100devicePixelRatio;
+        left:50%localStorage
+        padding: 20px;
+        border-Radius: 10px;
+        box-shadow: 0 10 px 5 px 0 #000;
+        margin:10px
+
+    `);
+    alert.innerHTML = `<span>${message}</span>`;
+    alert.appendChild(alertButton);
+  
+    };
+
+    document.body.appendChild(alert);
+
+
+
+
+console.log(result)*/
+
